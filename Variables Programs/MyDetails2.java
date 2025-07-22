c9b3a87f72f9149af353a722e75d9d641e91bfcd
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-22 09:27:46 +0530

    Updated variable assignment method in MyDetails2 program
    In the previous program, we declared variables first and assigned values separately. In this program, we directly assigned values to variables during declaration (parallel assignment). The program displays name, phone number, location, and grade.

---
+++ Variables Programs/MyDetails2.java
@@ -0,0 +1,14 @@
+class MyDetails2
+{
+	public static void main(String[] args) {
+		String name = "Sahil Phirke";
+		long phno = 9876543210l;
+		String place= "Pune";
+		char grade= 'A';
+
+		System.out.println("My Name is : "+name);
+		System.out.println("Phone Number : "+phno);
+		System.out.println("My Location is : "+place);
+		System.out.println("Grade is : "+grade);
+	}
+}
