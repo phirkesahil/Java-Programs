1e6da4c40f1e03122eab0b07718aea8e454b77bb
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-22 09:16:24 +0530

    Added Java program to display personal details using variables
    Displays personal details like name, age, phone number, salary, and grade using variables of various data types in Java.

---
+++ Variables Programs/MyDetails.java
@@ -0,0 +1,22 @@
+class MyDetails
+{
+	public static void main(String[] args) {
+		String name;
+		long phno;
+		byte age;
+		float sal;
+		char grade;
+
+		name="Sahil Phirke";
+		phno=9876543210l;
+		age=22;
+		sal=50000.0f;
+		grade='A';
+
+		System.out.println("My Name is : "+name);
+		System.out.println("My Age is :" +age);
+		System.out.println("Phone Number : "+phno);
+		System.out.println("Salary is : "+sal);
+		System.out.println("Grade is : "+grade);
+	}
+}
