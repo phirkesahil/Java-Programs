3e56001051c60cf570c36b786e19141c3329eaf9
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-22 19:56:04 +0530

    Added Java program for arithmetic operations with fixed values
    Created ArithmeticOperators class to perform basic arithmetic operations like addition, subtraction, multiplication, division, and modulus. The program uses two fixed integer values and displays the calculated results directly in the print statements.

---
+++ Arithmatic Operators/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulus: " + (num1 % num2));
+    }
+}
