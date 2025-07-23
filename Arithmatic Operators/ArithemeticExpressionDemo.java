49240db3695510e534d7c117e0f7bbc89431a59f
phirkesahil <sahilphirke04@gmail.com>
2025-07-23 07:14:52 +0530

    Deleted Arithmetic Operators directory for some ambiguous reason

--- Arithmatic Operators/ArithemeticExpressionDemo.java
+++
@@ -1,7 +0,0 @@
-public class ArithmeticExpressionDemo {
-    public static void main(String[] args) {
-        int result = 10 + 5 * 2 - 3 % 2;
-
-        System.out.println("Expression Result (10 + 5 * 2 - 3 % 2): " + result);
-    }
-}
