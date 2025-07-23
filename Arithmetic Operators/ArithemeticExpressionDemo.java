c19ee46e6ad844f6c8183a3a857f565e131b64c9
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-23 07:10:29 +0530

    Added Java program program to show arithmetic operator usage in expressions
    In this program, we used multiple arithmetic operators in a single expression. This shows that we can also use operators directly inside expressions to get results.

---
+++ Arithmetic Operators/ArithemeticExpressionDemo.java
@@ -0,0 +1,7 @@
+public class ArithmeticExpressionDemo {
+    public static void main(String[] args) {
+        int result = 10 + 5 * 2 - 3 % 2;
+
+        System.out.println("Expression Result (10 + 5 * 2 - 3 % 2): " + result);
+    }
+}
