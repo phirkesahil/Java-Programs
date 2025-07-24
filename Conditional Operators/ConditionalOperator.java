ec19eba176ef5cbd4199bbc8fd66ec8218a79e5c
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-24 07:09:09 +0530

    Added Java program to find the largest number using conditional operator
    This program compares two numbers using the conditional (ternary) operator ?: to find and print the largest number. It shows how we can use conditional operators as a shortcut for simple decision-making.

---
+++ Conditional Operators/ConditionalOperator.java
@@ -0,0 +1,11 @@
+class ConditionalOperator
+{
+	public static void main(String[] args) {
+		
+		int n1 = 100;
+		int n2 = 50;
+
+		int ans = n1>n2 ? n1 : n2;
+		System.out.println("The Lagrest Between "+ n1+ " and "+ n2+ " is "+ ans);
+	}
+}
