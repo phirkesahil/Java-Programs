cbef562b7fe82dad9f8625ed21bd08977633bfb6
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-25 22:05:17 +0530

    Added java program to demonstrate += operator with char type
    In this program, a character variable is incremented using the += compound assignment operator.

---
+++ CompoundOperators/CompoundUsingChar.java
@@ -0,0 +1,7 @@
+public class CompoundUsingChar {
+public static void main(String[] args) {
+        char letter = 'A';
+        letter += 2;
+        System.out.println("New Letter: " + letter);  // Output: C
+}
+}
