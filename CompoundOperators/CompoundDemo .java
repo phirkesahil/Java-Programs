97365f974d14c0cfd9ed471d46cb9a8f760d14f1
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-25 21:59:04 +0530

    Used compound operators with int and byte types
    In this program, we applied compound assignment (+=) to integer and byte variables. It shows how Java automatically handles type conversion during the operation.

---
+++ CompoundOperators/CompoundDemo .java
@@ -0,0 +1,11 @@
+class CompoundDemo {
+public static void main(String[] args) {
+	int sal=2000;
+	sal+=4000.50;
+	System.out.println(sal);
+
+	byte b = 127;
+	b+= 100;
+	System.out.println(b);
+}
+}
