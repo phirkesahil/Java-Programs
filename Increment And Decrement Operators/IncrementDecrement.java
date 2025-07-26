5376ae7a9e8127f3dad911218f8076452ecadf49
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-26 21:53:50 +0530

    Added basic  program using post-increment and post-decrement
    This program increases the value of 'a' by 1 and decreases the value of 'b' by 1 using increment and decrement operators.

---
+++ Increment And Decrement Operators/IncrementDecrement.java
@@ -0,0 +1,12 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        a++; // post-increment
+        b--; // post-decrement
+
+        System.out.println("a after increment: " + a);
+        System.out.println("b after decrement: " + b);
+    }
+}
