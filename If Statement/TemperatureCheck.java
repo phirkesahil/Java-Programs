9979f5acafe7043b41bd8e1fce5a90d49e7e7579
Sahil Phirke <sahilphirke04@gmail.com>
2025-07-27 23:02:12 +0530

    Added program to check for fever based on temperature using 'if statement'.
    This program uses an if statement to determine whether a person has a fever by checking if their temperature is greater than 37.0°C.

---
+++ If Statement/TemperatureCheck.java
@@ -0,0 +1,9 @@
+class TemperatureCheck {
+public static void main(String[] args) {
+        double temp = 38.5;
+
+        if (temp > 37.0) {
+            System.out.println("You have a fever.");
+        }
+           }
+}
